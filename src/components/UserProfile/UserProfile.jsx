73dce6c0ece468cb9036de2f2892d7f2c3155ecf
OscarKang1 <xodms9729@naver.com>
2025-05-20 15:12:50 +0900

    과제완료

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -2,6 +2,24 @@ import React from "react";
 import "./UserProfile.css";
 import lionImage from "./lion.jpeg";
 
-const UserProfile = () => {};
+const UserProfile = ({Name,Age,Depart}) => {
+    
+
+
+    return (
+        <div className="profile-card">
+            <div className = "profile-top">
+                <img src={lionImage} className = "profile-img"></img>
+            </div>
+            <div className = "profile-info">
+            <p>{Name}</p>
+            <p>나이:{Age}세</p>
+            <p>학과:{Depart}과</p>
+            </div>
+        </div>
+    );
+
+}
+
 
 export default UserProfile;
