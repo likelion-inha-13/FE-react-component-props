3591e7f9d23630d6d9dd64e4960a9864de1280ad
BBoyeon <2boyeon2@gmail.com>
2025-05-21 23:06:56 +0900

    react 과제

--- src/components/StaffProfile/StaffProfile.jsx
+++ src/components/StaffProfile/StaffProfile.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./StaffProfile.css";
 
-const StaffProfile = () => {
-  return <div></div>;
+const StaffProfile = (props) => {
+  return (
+  <div className="staff-card">
+    <h3>이름 : {props.name}</h3>
+    <p>파트 : {props.part}</p>
+  </div>);
+  
 };
 
 export default StaffProfile;
